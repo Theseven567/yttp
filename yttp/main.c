4685b4f2d4780d7960a1fb0bd244b0017c37ac1e
Yegor Chsherbatykh <yegor.chsherbatykg@nu.edu.kz>
2017-02-18 14:56:05 +0600

    Initial Commit

---
+++ yttp/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  yttp
+//
+//  Created by Егор on 2/18/17.
+//  Copyright © 2017 Yegor's Mac. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
